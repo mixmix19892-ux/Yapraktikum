6721052a0eeff765721a778ee1300c8866968b0f
mixmix19892@gmail.com <gnork666@mail.ru>
2026-05-20 22:42:02 +0300

    Доделай Практика 5-5

---
+++ Задачи/Untitled-1.py
@@ -0,0 +1,3 @@
+import random
+
+a = (i: random.randint(1, 100) for _ in range(100))
---
+++ Задачи/Спринт 2-3 - Тема 5-12 Практика 5-5.py
@@ -0,0 +1,37 @@
+def analyze_results(list_of_dicts: list[dict[str, int]]):
+    # Получите названия всех команд и отсортируйте их по алфавиту.
+    scores = {}
+
+    for dict in list_of_dicts:
+        for key in dict:
+            scores[key] = 0
+
+    for dict in list_of_dicts:
+        for key, value in dict.items():
+            scores[key] += value
+
+    scores = sorted(scores)
+    winner = None
+    winner_score = 0
+
+    for key, value in scores.items():
+        if scores[]
+
+    print('Команды, участвовавшие в чемпионате:')
+
+    for key in scores:
+        print(f'* {key}')
+
+    print(f'В чемпионате победила команда {winner} с результатом {winner_score} баллов')
+
+
+    
+
+
+races_data = [
+    {'Ferrari': 20, 'Mercedes': 5, 'Aston Martin': 10, 'Williams': 15},
+    {'Mercedes': 15, 'Aston Martin': 20, 'Ferrari': 10, 'Williams': 0},
+    {'Ferrari': 20, 'Williams': 15, 'Aston Martin': 10, 'Mercedes': 5}
+]
+
+analyze_results(races_data)
